e021628384b734ab699da490930c0a686135405d
TheBharatMishra <mishrabm2004@gmail.com>
2023-09-27 03:04:10 +0530

    Learning

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+import os
+from dotenv import load_dotenv
+# from langchain import HuggingFaceHub, LLMChain
+import requests
+from pydantic import model_validator
+
+
+load_dotenv()
+
+
+
+API_URL = "https://api-inference.huggingface.co/models/gpt2"
+headers = {"Authorization": f"Bearer {os.getenv('API_KEY')}"}
+
+
+# @model_validator(pre=False, skip_on_failure=True)
+def query(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+
+data = query("Can you please let us know more details about your ")
